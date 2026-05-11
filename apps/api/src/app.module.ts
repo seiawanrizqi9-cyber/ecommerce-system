74a981a63945c47073ef2a00723e70be77453760
Rizqi Setiawan <seiawanrizqi@gmail.com>
2026-05-12 06:28:29 +0700

    feat: testing modules and resolve error docker

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -2,9 +2,21 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './modules/auth/auth.module';
-
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 @Module({
-  imports: [AuthModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGODB_URI'),
+      }),
+    }),
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
