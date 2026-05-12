1fa2c1e123511c258a57a054f83c2ce2b024f208
Rizqi Setiawan <seiawanrizqi@gmail.com>
2026-05-12 20:44:23 +0700

    feat: add error handling for product

--- apps/api/src/modules/products/products.controller.ts
+++ apps/api/src/modules/products/products.controller.ts
@@ -1,4 +1,12 @@
-import { Body, Controller, Get, Post, Query, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Post,
+  Query,
+  UseGuards,
+} from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { QueryProductDto } from './dto/query-product.dto';
@@ -20,5 +28,10 @@ export class ProductsController {
   @Get()
   findAll(@Query() query: QueryProductDto) {
     return this.productsService.findAll(query);
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.productsService.findOne(id);
   }
 }
