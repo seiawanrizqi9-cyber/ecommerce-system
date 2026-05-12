8256b08aea66b21d32ffd3ea20746fb29e4fe1dc
Rizqi Setiawan <seiawanrizqi@gmail.com>
2026-05-12 21:04:12 +0700

    feat: add swagger for product

--- apps/api/src/modules/products/products.controller.ts
+++ apps/api/src/modules/products/products.controller.ts
@@ -7,17 +7,33 @@ import {
   Query,
   UseGuards,
 } from '@nestjs/common';
+
+import {
+  ApiBearerAuth,
+  ApiOperation,
+  ApiTags,
+  ApiProperty,
+} from '@nestjs/swagger';
+
 import { ProductsService } from './products.service';
+
 import { CreateProductDto } from './dto/create-product.dto';
 import { QueryProductDto } from './dto/query-product.dto';
-import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
-import { RolesGuard } from './decorators/roles.guard';
-import { Roles } from './decorators/roles.decorator';
 
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
+
+import { Roles } from './decorators/roles.decorator';
+import { RolesGuard } from './decorators/roles.guard';
+
+@ApiTags('Products')
 @Controller('products')
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
 
+  @ApiBearerAuth()
+  @ApiOperation({
+    summary: 'Create new product',
+  })
   @Post()
   @UseGuards(JwtAuthGuard, RolesGuard)
   @Roles('admin')
@@ -25,13 +41,24 @@ export class ProductsController {
     return this.productsService.create(createProductDto);
   }
 
+  @ApiOperation({
+    summary: 'Get all products',
+  })
   @Get()
   findAll(@Query() query: QueryProductDto) {
     return this.productsService.findAll(query);
   }
 
+  @ApiOperation({
+    summary: 'Get product by id',
+  })
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.productsService.findOne(id);
   }
+
+  @ApiProperty({
+    example: 'Gaming Mouse RGB',
+  })
+  name: string;
 }
