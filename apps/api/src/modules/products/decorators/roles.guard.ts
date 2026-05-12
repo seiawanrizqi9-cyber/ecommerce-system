4bec05364bcff6277c024598b06ac0f58e306026
Rizqi Setiawan <seiawanrizqi@gmail.com>
2026-05-12 20:09:18 +0700

    feat: add roles guard for product

---
+++ apps/api/src/modules/products/decorators/roles.guard.ts
@@ -0,0 +1,34 @@
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+
+import { Reflector } from '@nestjs/core';
+
+import { Request } from 'express';
+
+import { ROLES_KEY } from './roles.decorator';
+import { RequestUser } from '../../../common/interfaces/request-user.interface';
+
+@Injectable()
+export class RolesGuard implements CanActivate {
+  constructor(private readonly reflector: Reflector) {}
+
+  canActivate(context: ExecutionContext): boolean {
+    const requiredRoles = this.reflector.getAllAndOverride<string[]>(
+      ROLES_KEY,
+      [context.getHandler(), context.getClass()],
+    );
+
+    if (!requiredRoles) {
+      return true;
+    }
+
+    const request = context.switchToHttp().getRequest<
+      Request & {
+        user: RequestUser;
+      }
+    >();
+
+    const user = request.user;
+
+    return requiredRoles.includes(user.role);
+  }
+}
--- apps/api/src/modules/products/products.controller.ts
+++ apps/api/src/modules/products/products.controller.ts
@@ -1,13 +1,18 @@
-import { Body, Controller, Get, Post, Query } from '@nestjs/common';
+import { Body, Controller, Get, Post, Query, UseGuards } from '@nestjs/common';
 import { ProductsService } from './products.service';
-import { QueryProductDto } from './dto/query-product.dto';
 import { CreateProductDto } from './dto/create-product.dto';
+import { QueryProductDto } from './dto/query-product.dto';
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
+import { RolesGuard } from './decorators/roles.guard';
+import { Roles } from './decorators/roles.decorator';
 
 @Controller('products')
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
 
   @Post()
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles('admin')
   create(@Body() createProductDto: CreateProductDto) {
     return this.productsService.create(createProductDto);
   }
