a3f1c2c732b36be57ba0d1f78382b42882b0c946
Rizqi Setiawan <seiawanrizqi@gmail.com>
2026-05-13 13:38:27 +0700

    feat: update auth swagger

--- apps/api/src/modules/auth/dto/refresh-token.dto.ts
+++ apps/api/src/modules/auth/dto/refresh-token.dto.ts
@@ -1,6 +1,10 @@
 import { IsNotEmpty, IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class RefreshTokenDto {
+  @ApiProperty({
+    example: 'eyJhbGciOiJIUzI1NiIsInR5cCI6...',
+  })
   @IsString()
   @IsNotEmpty()
   refreshToken: string;
