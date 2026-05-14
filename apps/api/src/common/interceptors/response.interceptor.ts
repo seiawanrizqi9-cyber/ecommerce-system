8454d841cf03215708757791d4fa09ebde03447a
Rizqi Setiawan <seiawanrizqi@gmail.com>
2026-05-14 12:48:54 +0700

    feat: add Global Exception Filter and Response Interceptor, then update main.ts

---
+++ apps/api/src/common/interceptors/response.interceptor.ts
@@ -0,0 +1,32 @@
+import {
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  NestInterceptor,
+} from '@nestjs/common';
+import { Observable, map } from 'rxjs';
+
+interface ApiResponse<T> {
+  success: boolean;
+  message: string;
+  data: T;
+}
+
+@Injectable()
+export class ResponseInterceptor<T> implements NestInterceptor<
+  T,
+  ApiResponse<T>
+> {
+  intercept(
+    _context: ExecutionContext,
+    next: CallHandler,
+  ): Observable<ApiResponse<T>> {
+    return next.handle().pipe(
+      map((data: T) => ({
+        success: true,
+        message: 'Request successful',
+        data,
+      })),
+    );
+  }
+}
