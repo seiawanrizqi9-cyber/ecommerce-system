d86a66b3de72d3891b9b91df86810633ca9a174f
Rizqi Setiawan <seiawanrizqi@gmail.com>
2026-05-14 12:57:52 +0700

    feat: add environment validation in app.module and logging improvement in main.ts

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -6,11 +6,19 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { OrderModule } from './modules/order/order.module';
 import { BullModule } from '@nestjs/bullmq';
+import * as Joi from 'joi';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+      validationSchema: Joi.object({
+        PORT: Joi.number().default(3000),
+        MONGODB_URI: Joi.string().required(),
+        JWT_SECRET: Joi.string().min(6).required(),
+        REDIS_HOST: Joi.string().required(),
+        REDIS_PORT: Joi.number().required(),
+      }),
     }),
 
     MongooseModule.forRootAsync({
@@ -20,11 +28,14 @@ import { BullModule } from '@nestjs/bullmq';
       }),
     }),
 
-    BullModule.forRoot({
-      connection: {
-        host: process.env.REDIS_HOST,
-        port: Number(process.env.REDIS_PORT),
-      },
+    BullModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        connection: {
+          host: configService.get<string>('REDIS_HOST'),
+          port: configService.get<number>('REDIS_PORT'),
+        },
+      }),
     }),
 
     AuthModule,
